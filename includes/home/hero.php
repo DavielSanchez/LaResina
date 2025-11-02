f6020fd84c990e62ad3a12610562cd98da2aadc1
Daviel_Sanchez <Davielsanchez14@gmail.com>
2025-11-02 18:00:15 -0500

    Boton de reservacion

--- includes/home/hero.php
+++ includes/home/hero.php
@@ -5,7 +5,7 @@
                 <h1 data-aos="fade-up">Bienvenidos a<br>La Resina<span class='text-danger' >.</span></h1>
                 <p data-aos="fade-up" data-aos-delay="100">Donde la tradición y el sabor se encuentran. Experiencias culinarias únicas con el toque especial de nuestra casa.</p>
                 <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-                    <a href="#book-a-table" class="btn-get-started">Reservar Mesa</a>
+                    <a href="reservation.php" class="btn-get-started">Reservar Mesa</a>
                     <a href="https://youtu.be/VJMaqPko-fA" class="glightbox btn-watch-video d-flex align-items-center">
                         <i class="bi bi-play-circle"></i>
                         <span>Conoce La Resina<span class='text-danger' >.</span></span>
